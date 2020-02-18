70bbc87879c20b429127cdf9f8e51147a6a0ec9e
FABIO KENJI HAGIWARA <180106@facens.br>
2020-02-17 21:53:48 -0300

    a

--- src/app/App.java
+++ src/app/App.java
@@ -3,6 +3,5 @@ package app;
 public class App {
     public static void main(String[] args) throws Exception {
         System.out.println("Hello Java");
-        System.out.println("Teste Git");
     }
 }
